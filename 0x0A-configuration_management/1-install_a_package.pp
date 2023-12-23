9363285e510c1886e5add0d4fb6e1605f1748927
mohammedmxz <mohammedmxzk@gmail.com>
2023-12-23 01:21:09 +0000

    just added

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+# install flask from pip3.
+
+package { 'flask':
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3',
+}
